b07c99d0e25b18681f6401ae768b08a517cf47cc
Andrei <70886263+selfc0ntrol@users.noreply.github.com>
2022-04-18 21:55:19 +0300

    Add files via upload

---
+++ Proxy/Proxy/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proxy
+{
+    class Program
+    {
+        static void Main()
+        {
+            IPayment cash = new Cash();
+            IPayment creditCard = new CreditCard(cash);
+            creditCard.Pay();
+            Console.ReadKey();
+        }
+    }
+    interface IPayment
+    {
+        void Pay();
+    }
+    class Cash : IPayment
+    {
+        public void Pay()
+        {
+            Console.WriteLine("Plată finalizată");
+        }
+    }
+    class CreditCard : IPayment
+    {
+        IPayment @cash;
+        public CreditCard(IPayment @cash)
+        {
+            this.@cash = @cash;
+        }
+        public void Pay()
+        {
+            this.@cash.Pay();
+        }
+    }
+
+}
